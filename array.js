d4e81b6ce594219e5d138ea59fa4395a73b3e6cb
Jess Doyle <jessica.e.doyle@gmail.com>
2019-10-02 12:01:31 -0400

    base methods complete

--- array.js
+++ array.js
@@ -28,6 +28,42 @@ class MyArray {
     this.capacity = size;
   }
 
+  get(index) {
+    if (index <0 || index > this.length-1) {
+      throw new Error ('Index out of bounds!');
+    }
+    return memory.get(this.ptr+index);
+  }
+
+  pop() {
+    if (this.length <= 0) {
+      throw new Error ('Cannot pop() an empty array!');
+    }
+    const lastItem = this.get(this.length-1);
+    this.length --;
+    return lastItem;
+  }
+
+  insert (index, value) {
+    if (index < 0 || index > this.length-1) {
+      throw new Error ('Index out of bounds!');
+    }
+    if (this.length >= this.capacity) {
+      this.resize((this.length+1)* MyArray.SIZE_RATIO);
+    }
+    memory.copy(this.ptr + index + 1, this.ptr + index, this.length - index);
+    memory.set(this.ptr+index, value);
+    this.length++;
+  }
+
+  remove (index) {
+    if (index < 0 || index > this.length-1) {
+      throw new Error ('Index out of bounds!');
+    }
+    memory.copy(this.ptr + index, this.ptr + index + 1, this.length - index - 1);
+    this.length--;
+  }
+
 }
 MyArray.SIZE_RATIO = 3;
 
--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 'use strict';
 
 const MyArray = require ('./array');
-const Memory = require('./memory');
 
 let arr = new MyArray();
 
-arr.push(2);
+console.log(arr);
+
+//What is the length, capacity and memory address of your array?
+//Length 0, capacity 0, memory address 0
+
+arr.push(5);
+arr.push(15);
+arr.push(19);
+arr.push(45);
+arr.push(10);
 
 console.log(arr);
-console.log(arr[0]);
+
+//What is the length, capacity and memory address of your array?
+//Explain the result of your program after adding the new lines of code.
+//Length 5, capacity 12, memory address 3
+//Length increased to 5 due to 5 push commands
+//Capacity was increased twice, once to 3 after the first push
+// and once to 12 after the fourth push, in order to account for the growing array length
+// in memory.
+//Memory address was changed from 0 to 3 after the fourth push (the second resizing of the array).
+
+
+console.log(arr.pop());
+console.log(arr.pop());
+console.log(arr.pop());
+
+console.log(arr);
+
+//What is the length, capacity, and address of your array? 
+//Explain the result of your program after adding the new lines of code.
+//Length is now 2 (removed 3 items)
+//Memory address and capacity are the same
